b126df6b64ded3b627ba45b115fb89b5449c4da6
Carlton Bickford <carltonbickford@gmail.com>
2018-12-20 21:06:32 -0700

    fix rounding

--- report/BoostersActivityReport.php
+++ report/BoostersActivityReport.php
@@ -76,8 +76,40 @@ class BoostersActivityReport extends ActivityReport {
         
         return pg_fetch_all($result);
     }
+       
+    private function getReloadsOfActiveStudents(&$count, &$sum) {
+        
+        $result = pg_query_params(
+            "SELECT ks.reload_date, ks.card, st.first, st.middle, st.last, ks.reload_amount, st.id " .
+            "FROM ks_card_reloads AS ks INNER JOIN students AS st " . 
+            "ON ks.student = st.id " .
+            "WHERE ks.reload_date>=$1 AND ks.reload_date<=$2" .
+            "ORDER BY st.last, st.first, st.middle, ks.reload_date",
+            array($this->startDate, $this->endDate));
+             
+        if (!$result) {
+            throw new Exception(pg_last_error());
+        }
+        
+        $count = pg_num_rows($result);
+        
+        $sumResult = pg_query_params(
+            "SELECT SUM(ks.reload_amount) FROM ks_card_reloads AS ks " .
+            "INNER JOIN students AS st " . 
+            "ON ks.student = st.id " .
+            "WHERE ks.reload_date>=$1 AND ks.reload_date<=$2",
+            array($this->startDate, $this->endDate));
+        
+        if (!$sumResult) {
+            throw new Exception(pg_last_error());
+        }
+        
+        $sum = pg_fetch_result($sumResult, 0, 0);
+        
+        return pg_fetch_all($result);
+    }
     
-        private function getReloadsOfInactiveStudent($studentId) {
+    private function getReloadsOfInactiveStudent($studentId) {
         // Get all card reloads for the student in the date range
         $result = pg_query_params(
             "SELECT ks.reload_date, ks.card, st.first, st.middle, st.last, ks.reload_amount " . 
@@ -157,28 +189,47 @@ class BoostersActivityReport extends ActivityReport {
     
     protected function writeActiveStudentKsReloads() {
        
-        $involvedActiveStudents = $this->getInvolvedActiveStudents();
+        $includeStudentSubtotals = false;
         $grandTotal = 0;
-        foreach ($involvedActiveStudents as $student) {
-            $this->writeStudentCardHeaders();
-            $studentTotal = 0;
-            $id = $student['student'];
-            $reloads = $this->getReloadsOfActiveStudent($id);
+        
+        if (!$includeStudentSubtotals) {
+            $count = 0;
+            $sum = 0;
+            $reloads = $this->getReloadsOfActiveStudents($count, $sum);
+            $grandTotal = $sum;
+            $this->writeCategoryHeader("King Soopers reloads with active student", $count);
             foreach ($reloads as $reload) {
                 $this->writeCardReload(
-                        $reload['reload_date'],
-                        $reload['card'],
-                        $reload['first'] . " " . $reload['middle'] . " " .  $reload['last'] ,   
-                        $reload['reload_amount']);
-                $studentTotal += $reload['reload_amount'];
+                    $reload['reload_date'],
+                    $reload['card'],
+                    $reload['first'] . " " . $reload['middle'] . " " .  $reload['last'] ,   
+                    $reload['reload_amount']);
             }
-            $this->writeStudentCardsTotal($studentTotal, true);
-            $grandTotal += $studentTotal;
+        }
+        else {
+            $involvedActiveStudents = $this->getInvolvedActiveStudents();
+            foreach ($involvedActiveStudents as $student) {
+                $this->writeStudentCardHeaders();
+                $studentTotal = 0;
+                $id = $student['student'];
+                $reloads = $this->getReloadsOfActiveStudent($id);
+                foreach ($reloads as $reload) {
+                    $this->writeCardReload(
+                            $reload['reload_date'],
+                            $reload['card'],
+                            $reload['first'] . " " . $reload['middle'] . " " .  $reload['last'] ,   
+                            $reload['reload_amount']);
+                    $studentTotal += $reload['reload_amount'];
+                }
+                $this->writeStudentCardsTotal($studentTotal, true);
+                $grandTotal += $studentTotal;
+            }
         }
         $this->writeUnderline();
+        $this->writeSubTotalHeaders();
         $this->writeCardsTotal("Total for Active Students", $grandTotal, true);
     }
-    
+       
     protected function writeInactiveStudentKsReloads() {
        
         $involvedInactiveStudents = $this->getInvolvedInactiveStudents();
@@ -208,7 +259,6 @@ class BoostersActivityReport extends ActivityReport {
     }
     
     private function getUnknownCardKsReloads(&$count, &$sum) {
-        // select ks.reload_date, ks.card, ks.reload_amount, ks.original_invoice_number, ks.original_invoice_date from ks_card_reloads as ks left join cards as c on ks.card = c.id where c.id is null order by ks.card, ks.reload_date
         $result = pg_query_params(
             "SELECT ks.reload_date, ks.card, ks.reload_amount, " .
             "ks.original_invoice_number, ks.original_invoice_date " .
@@ -269,13 +319,12 @@ class BoostersActivityReport extends ActivityReport {
         $styleRa  = "class='tg-ra'";
         $styleB3sl = "class='tg-b3sl'";
         $styleR3sl = "class='tg-r3sl'";
-        $styleB3sr = "class='tg-b3sr'";
-        $styleR3sr = "class='tg-r3sr'";
                
         $rebate = $total * RebatePercentages::$KS_CARD_RELOAD;
         if ($studentGetsShare) {
             $studentShare = $rebate * RebatePercentages::$STUDENT_SHARE;
-            $boostersShare = $rebate - $studentShare;
+            $studentShareAmt = $this->format($studentShare);
+            $boostersShare = $rebate - $studentShareAmt;
             
         }
         else {
@@ -314,14 +363,13 @@ class BoostersActivityReport extends ActivityReport {
         $styleRa  = "class='tg-ra'";
         $styleB3sl = "class='tg-b3sl'";
         $styleR3sl = "class='tg-r3sl'";
-        $styleB3sr = "class='tg-b3sr'";
-        $styleR3sr = "class='tg-r3sr'";
                
         $rebate = $total * RebatePercentages::$KS_CARD_RELOAD;
+        $rebate = round($rebate, 2);
         if ($studentGetsShare) {
             $studentShare = $rebate * RebatePercentages::$STUDENT_SHARE;
+            $studentShare = round($studentShare, 2);
             $boostersShare = $rebate - $studentShare;
-            
         }
         else {
             $boostersShare = $rebate;
@@ -357,9 +405,9 @@ class BoostersActivityReport extends ActivityReport {
     {
         $styleRa = "class='tg-ra'";
         
-        $rebate = $amount * RebatePercentages::$KS_CARD_RELOAD;
+        //$rebate = $amount * RebatePercentages::$KS_CARD_RELOAD;
         $amountStr = $this->format($amount);
-        $rebateStr = $this->format($rebate);
+        //$rebateStr = $this->format($rebate);
         
         $this->table .=
         "<tr>" .
@@ -367,7 +415,7 @@ class BoostersActivityReport extends ActivityReport {
             "<td>$card</td>" .
             "<td>$student</td>" .
             "<td $styleRa>$amountStr</td>" .
-            "<td $styleRa>$rebateStr</td>" .
+            "<td></td>" .
             "<td></td>" .
             "<td></td>" .
         "</tr>";
@@ -399,7 +447,7 @@ class BoostersActivityReport extends ActivityReport {
         $this->startTable();
         $this->writeNameDateTitle("Boosters Activity");
         if ($this->includeKsCards) {
-            $this->writeCategoryHeader("King Soopers reloads with active student");
+            
             $this->writeActiveStudentKsReloads();
             //$this->writeCategoryHeader("King Soopers reloads with inactive student");
             //$this->writeInactiveStudentKsReloads();
